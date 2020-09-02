e116655568018f6129448d8b67d2ce2ccf0fab80
andrey <xamidylin@gismeteo.com>
2020-09-02 14:25:22 +0300

    shared store for workers

--- packages/domain/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './src/model.proxy';
-export * from './src/model.stream';
-export * from './src/model';
-export * from './src/webworker.entry';
-export * from './src/websocket.entry';
-export * from './src/container';
-export * from './helpers/DomainError';
