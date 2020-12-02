746b066809e9a55d297f3d9cd655a74e82cf7caa
Alexey Baranov <baranov@gismeteo.com>
2020-12-02 19:02:09 +0300

    fix ModelProxy.Actions as thenable

--- packages/domain/src/model.proxy.ts
+++ packages/domain/src/model.proxy.ts
@@ -23,6 +23,8 @@ export class ModelProxy<TState, TActions extends IActions<TActions>> {
       // для примитивов
       if (typeof key !== "string")
         return () => null;
+      if (key == "then")
+        return Promise.resolve(target);
       return target[key] || (target[key] = (async (...args) => {
         try {
           const res = await this.stream.Action({
