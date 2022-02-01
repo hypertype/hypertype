c8551c8d1a192de8f2b277d672669e9a3b755d8e
Andrey <andrey-ne@mail.ru>
2022-02-01 15:29:13 +0300

    f

--- packages/tools/types.d.ts
+++ packages/tools/types.d.ts
@@ -7,3 +7,18 @@ declare global {
     }
   }
 }
+
+declare module "*.less" {
+  const style: string;
+  export default style;
+}
+
+declare module "*.css" {
+  const style: string;
+  export default style;
+}
+
+declare module "*.html" {
+  const html: string;
+  export default html;
+}
