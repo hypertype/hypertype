00fdf9a36eafc4f924eca60b08633c3d1f361aa6
Andrey <andrey-ne@mail.ru>
2022-02-01 15:45:06 +0300

    fix typings

--- packages/tools/types.d.ts
+++ packages/tools/types.d.ts
@@ -7,18 +7,3 @@ declare global {
     }
   }
 }
-
-declare module "*.less" {
-  const style: string;
-  export default style;
-}
-
-declare module "*.css" {
-  const style: string;
-  export default style;
-}
-
-declare module "*.html" {
-  const html: string;
-  export default html;
-}
---
+++ packages/tools/typings.d.ts
@@ -0,0 +1,15 @@
+
+declare module "*.less" {
+  const style: string;
+  export default style;
+}
+
+declare module "*.css" {
+  const style: string;
+  export default style;
+}
+
+declare module "*.html" {
+  const html: string;
+  export default html;
+}
