cb49a16a4378d12206ecf9b0e114da3fd1f0cb1b
gonzobard777 <gonzo.bard@gmail.com>
2022-02-15 15:28:49 +0300

    compare

--- packages/core/utils/Fn.ts
+++ packages/core/utils/Fn.ts
@@ -72,17 +72,65 @@ export const Fn = {
       return false;
     if (a.equals && b.equals)
       return a.equals(b);
-    if (Array.isArray(a) && Array.isArray(b)) {
-      return a.length === b.length &&
-        a.every((x, i) => compare(x, b[i]));
-    }
+
+    const aIsArr = Array.isArray(a);
+    const bIsArr =  Array.isArray(b);
+    if (aIsArr && bIsArr)
+      return compareArrays(a, b);
+    else if(aIsArr || bIsArr)
+      return false;
+
+    const asIsSet = a instanceof Set;
+    const bsIsSet = b instanceof Set;
+    if (asIsSet && bsIsSet)
+      return compareSets(a, b);
+    else if (asIsSet || bsIsSet)
+      return false;
+
+    const aIsMap = a instanceof Map;
+    const bIsMap = b instanceof Map;
+    if (aIsMap && bIsMap)
+      return compareMaps(a, b);
+    else if (aIsMap || bIsMap)
+      return false;
+
     if (typeof a === "object" && typeof b === "object") {
       const aKeys = Object.getOwnPropertyNames(a);
       const bKeys = Object.getOwnPropertyNames(b);
-      if (!compare(aKeys, bKeys))
-        return false;
-      return aKeys.every(key => compare(a[key], b[key]));
+      return compareArraysOfPrimitives(aKeys, bKeys)
+        && aKeys.every(key => compare(a[key], b[key]));
     }
     return false;
   }
 };
+
+
+//region Compare
+
+function compareArrays(a: any[], b: any[]): boolean {
+  return a.length === b.length &&
+    a.every((x, i) => Fn.compare(x, b[i]));
+}
+
+function compareArraysOfPrimitives(a: any[], b: any[]): boolean {
+  return a.length === b.length &&
+    a.every((x, i) => x === b[i]);
+}
+
+function compareSets(a: Set<any>, b: Set<any>): boolean {
+  return a.size === b.size
+    && compareArrays(Array.from(a).sort(), Array.from(b).sort());
+}
+
+function compareMaps(a: Map<any, any>, b: Map<any, any>): boolean {
+  if (a.size !== b.size)
+    return false;
+  const aKeys = Array.from(a.keys()).sort();
+  const bKeys = Array.from(b.keys()).sort();
+  return compareArrays(aKeys, bKeys)
+    && aKeys.every((aKey, i) =>
+      Fn.compare(a.get(aKey), b.get(bKeys[i]))
+    );
+}
+
+//endregion
