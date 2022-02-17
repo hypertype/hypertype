e07a5b087df8b46d26ab15364e44de64b07fbdd4
Andrey <andrey-ne@mail.ru>
2022-02-17 12:25:16 +0300

    fix compile ts

--- packages/tools/plugins/absolute/absolute-plugin.cjs
+++ packages/tools/plugins/absolute/absolute-plugin.cjs
@@ -2,7 +2,7 @@ const ts = require("typescript");
 const path = require("path");
 const fs = require("fs");
 
-function visitExportNode(exportNode, sourceFile) {
+function visitExportNode(exportNode, sourceFile, options) {
   if (exportNode.typeOnly) {
     console.log('type olnly')
     return;
@@ -13,36 +13,40 @@ function visitExportNode(exportNode, sourceFile) {
   const sourceFileDir = path.dirname(sourceFile.path);
   const abs = path.resolve(sourceFileDir, file);
   if (/\.(less|css|scss|sass|svg|png|html)$/.test(file)) {
-    return ts.updateExportDeclaration(exportNode, exportNode.decorators, exportNode.modifiers, exportNode.exportClause, ts.createStringLiteral(abs), exportNode.typeOnly);
+    const absSource = path.join(options.outDir, path.relative(options.baseUrl, sourceFileDir));
+    const relFile = path.relative(absSource, abs).replaceAll(path.sep, '/');
+    return ts.updateExportDeclaration(exportNode, exportNode.decorators, exportNode.modifiers, exportNode.exportClause, ts.createStringLiteral(relFile), exportNode.typeOnly);
   }
   if (fs.existsSync(abs + '.ts')) {
     return ts.updateExportDeclaration(exportNode, exportNode.decorators, exportNode.modifiers, exportNode.exportClause, ts.createStringLiteral(file + '.js'), exportNode.typeOnly);
   }
-  if (fs.existsSync(abs + '/')) {
-    const indexFile = `./${file}/index.js`;
+  if (fs.existsSync(abs) && fs.statSync(abs).isDirectory()) {
+    const indexFile = `${file}/index.js`;
     return ts.updateExportDeclaration(exportNode, exportNode.decorators, exportNode.modifiers, exportNode.exportClause, ts.createStringLiteral(indexFile), exportNode.typeOnly);
   }
 }
 
-function visitImportNode(importNode, sourceFile) {
+function visitImportNode(importNode, sourceFile, options) {
   const file = importNode.moduleSpecifier?.text;
   if (!file || !file.startsWith('.'))
     return;
   const sourceFileDir = path.dirname(sourceFile.path);
   const abs = path.resolve(sourceFileDir, file);
   if (/\.(less|css|scss|sass|svg|png|html)$/.test(file)) {
-    return ts.updateImportDeclaration(importNode, importNode.decorators, importNode.modifiers, importNode.importClause, ts.createStringLiteral(abs), importNode.assertClause);
+    const absSource = path.join(options.outDir, path.relative(options.baseUrl, sourceFileDir));
+    const relFile = path.relative(absSource, abs).replaceAll(path.sep, '/');
+    return ts.updateImportDeclaration(importNode, importNode.decorators, importNode.modifiers, importNode.importClause, ts.createStringLiteral(relFile), importNode.assertClause);
   }
   if (fs.existsSync(abs + '.ts')) {
     return ts.updateImportDeclaration(importNode, importNode.decorators, importNode.modifiers, importNode.importClause, ts.createStringLiteral(file + '.js'), importNode.assertClause);
   }
-  if (fs.existsSync(abs + '/')) {
-    const indexFile = `./${file}/index.js`;
+  if (fs.existsSync(abs) && fs.statSync(abs).isDirectory()) {
+    const indexFile = `${file}/index.js`;
     return ts.updateImportDeclaration(importNode, importNode.decorators, importNode.modifiers, importNode.importClause, ts.createStringLiteral(indexFile), importNode.assertClause);
   }
 }
 
-function visitRequireNode(importNode, sourceFile) {
+function visitRequireNode(importNode, sourceFile, options) {
   if (!(importNode.expression.kind === ts.SyntaxKind.Identifier &&
     importNode.expression.escapedText === "require")) {
     return;
@@ -50,12 +54,15 @@ function visitRequireNode(importNode, sourceFile) {
   const file = importNode.arguments[0].text;
   if (/\.(less|css|scss|sass|svg|png|html)/.test(file)) {
     const sourceFileDir = path.dirname(sourceFile.path);
-    const real = path.join(sourceFileDir, file);
-    return ts.updateCall(importNode, importNode.expression, undefined, [ts.createStringLiteral(real)]);
+    const abs = path.join(sourceFileDir, file);
+    const absSource = path.join(options.outDir, path.relative(options.baseUrl, sourceFileDir));
+    const relFile = path.relative(absSource, abs).replaceAll(path.sep, '/');
+    return ts.updateCall(importNode, importNode.expression, undefined, [ts.createStringLiteral(relFile)]);
   }
 }
 
 const lessToStringTransformer = function (context) {
+  const options = context.getCompilerOptions();
   return (sourceFile) => {
     function visitor(node) {
       // if (node && node.kind == ts.SyntaxKind.ImportDeclaration) {
@@ -64,17 +71,17 @@ const lessToStringTransformer = function (context) {
       if (!node)
         return ts.visitEachChild(node, visitor, context);
       if (ts.isCallExpression(node)) {
-        const result = visitRequireNode(node, sourceFile);
+        const result = visitRequireNode(node, sourceFile, options);
         if (result)
           return result;
       }
       if (ts.isImportDeclaration(node)) {
-        const result = visitImportNode(node, sourceFile);
+        const result = visitImportNode(node, sourceFile, options);
         if (result)
           return result;
       }
       if (ts.isExportDeclaration(node)) {
-        const result = visitExportNode(node, sourceFile);
+        const result = visitExportNode(node, sourceFile, options);
         if (result)
           return result;
       }
